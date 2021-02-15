19bf0078c1ea986f50fc972ed0bd7cebee9b0ac5
pavlo <pavlo@pavlo-virtual-machine>
2021-02-15 14:21:47 +0200

    Initial scratch project

---
+++ Task1-Stack/Task1-Stack.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Task1-Stack.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
