e9925e3e49e95306406c9b264c23bcbb39bf2bfa
pavlo <pavlo@pavlo-virtual-machine>
2021-02-16 19:30:57 +0200

    Fibonacci recursive calculation

--- Task2-Fibonacci/Task2-Fibonacci.cpp
+++ Task2-Fibonacci/Task2-Fibonacci.cpp
@@ -11,16 +11,46 @@
 
 using namespace std;
 
-
+uint sum_fibonacci(uint , uint , uint );
 
 int main(void) {
 
-	string line;
+	string 	line;
+	ulong	n;
 
-    cout << "HELLO > " && getline(cin, line);
+    cout << "Enter amount of Fibonacci numbers > ";
+    getline(cin, line);
 
+    n=stoul(line);
+    cout << "You ask for " << n << " numbers" << endl;
+
+    switch(n){
+    case 0:
+    case 1:
+
+    	break;
+    case 2:
+
+    	break;
+    default:
+        sum_fibonacci(0,1,n-3);
+    }
 
     cout << "Goodbye.\n";
 	return EXIT_SUCCESS;
 }
 
+// Fibonacci recursion calculation
+// i - actual element
+// j - next element
+// n - progression's element number limit
+uint sum_fibonacci(uint i, uint j, uint n){
+
+	cout << "next element " << i << " + " << j << " = " << i+j << endl;
+
+	if(n){
+		sum_fibonacci(j, i+j, --n);
+	}
+
+	return 0;
+}
