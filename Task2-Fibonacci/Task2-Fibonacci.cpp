2721388038f3b6735f941cd286a8b2579f1a7955
pavlo <pavlo@pavlo-virtual-machine>
2021-02-16 19:03:27 +0200

    Initial command line template

---
+++ Task2-Fibonacci/Task2-Fibonacci.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Task1-Stack.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(void) {
+
+	string line;
+
+    cout << "HELLO > " && getline(cin, line);
+
+
+    cout << "Goodbye.\n";
+	return EXIT_SUCCESS;
+}
+
