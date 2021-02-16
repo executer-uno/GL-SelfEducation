6abfe0ba543b7c0f95afe9d7b5d36209e736abd8
pavlo <pavlo@pavlo-virtual-machine>
2021-02-16 15:09:12 +0200

    command line interface added

--- Task1-Stack/Task1-Stack.cpp
+++ Task1-Stack/Task1-Stack.cpp
@@ -7,40 +7,52 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
 
 #include "stack.h"
 
 using namespace std;
 
+stack MyStack;
+
+void process(string const & line){		// https://stackoverflow.com/questions/13168833/c-command-line-interface
+
+	switch (line){
+		case "create":
+			MyStack.create(10);
+			break;
+		case "push":
+			MyStack.push(11);
+			break;
+		case "pop":
+			cout << MyStack.pop() << endl;
+			break;
+		case "resize":
+			MyStack.resize(MyStack.size()*2);
+			break;
+		case "help":
+		default:
+			cout << "Stack usage manual:" << endl;
+			cout << "create n - creates stack with n elements capacity" << endl;
+			cout << "push n - add element n to stack" << endl;
+			cout << "pop - retrieves element from stack" << endl;
+			cout << "resize - resize stack for new capacity" << endl;
+			cout << "PS: all illogical or data lose operations will throw exceptions" << endl;
+			break;
+	}
+
+
+}
+
 int main(void) {
 
-	stack MyStack;
+    for (string line; cout << "APP > " && getline(cin, line); )
+    {
+        if (!line.empty()) {
+        	process(line);
+        }
+    }
 
-	MyStack.create(10);
-
-	for(int i=0; i<10; i++){
-		MyStack.push(i);
-	}
-
-	for(int i=0; i<10; i++){
-		cout << MyStack.pop() << endl;
-	}
-
-	for(int i=10; i<20; i++){
-		MyStack.push(i);
-	}
-
-	MyStack.resize(MyStack.size()*2);
-
-	for(int i=10; i<20; i++){
-		MyStack.push(i);
-	}
-
-	for(int i=0; i<20; i++){
-		cout << MyStack.pop() << endl;
-	}
-
-	cout << "Done" << endl;
-
+    std::cout << "Goodbye.\n";
 	return EXIT_SUCCESS;
 }
